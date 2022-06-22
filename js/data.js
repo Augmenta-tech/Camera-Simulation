493ee20b1d4ff00d8809863e9358243156a88a88
Baptiste Desgouttes <baptiste.desgouttes@gmail.com>
2022-06-22 14:09:23 +0200

    add femto w

--- js/data.js
+++ js/data.js
@@ -1,7 +1,7 @@
 const camerasTypes = 
 [
     {
-        id:0, 
+        //id:0, 
         name:"Orbbec Astra +", 
         HFov:56.5, 
         VFov:45, 
@@ -16,7 +16,7 @@ const camerasTypes =
         recommended: true
     },
     {
-        id:1, 
+        //id:1, 
         name:"Orbbec Astra Pro", 
         HFov:60, 
         VFov:49.5, 
@@ -31,7 +31,7 @@ const camerasTypes =
         recommended: true
     },
     {
-        id:2, 
+        //id:2, 
         name:"Azure Kinect (NFOV)", 
         // inscribed square. 75°x65° are official FOV for the hexagon
         HFov: Math.round(2 * (Math.atan((1/3) * ( - Math.tan(75/2 * Math.PI/180) + Math.sqrt(3*Math.tan(65/2 * Math.PI/180)*Math.tan(65/2 * Math.PI/180) + 4*Math.tan(75/2 * Math.PI/180)*Math.tan(75/2 * Math.PI/180))))) * 180/Math.PI * 10) / 10, 
@@ -47,7 +47,7 @@ const camerasTypes =
         recommended: true
     },
     {
-        id:3, 
+        //id:3, 
         name:"Azure Kinect (WFOV)", 
         // inscribed square. 120° are the FOV for the circle. 
         // tan(squareFOV / 2) = sqrt(2)/2 * tan(circleFOV/2)
@@ -65,7 +65,7 @@ const camerasTypes =
         recommended: true
     },
     {
-        id:4, 
+        //id:4, 
         name:"Orbbec Femto", 
         HFov:64.6, 
         VFov:50.8, 
@@ -80,7 +80,7 @@ const camerasTypes =
         recommended: true
     },
     {
-        id:5, 
+        //id:5, 
         name:"Helios2", 
         HFov:69, 
         VFov:51, 
@@ -93,10 +93,28 @@ const camerasTypes =
         ],
         checkedDefault : false,
         recommended: true
+    },
+    {
+        //id:4, 
+        name:"Orbbec Femto W", 
+        HFov:90, 
+        VFov:74, 
+        rangeNear: 0.25,
+        rangeFar: 2.5,
+        handFar: 2.5,
+        suitable: [
+            "human-tracking",
+            "hand-tracking" 
+        ],
+        checkedDefault : false,
+        recommended: true
     }
 ];
 
+let i = 0;
+camerasTypes.forEach(type => type.id = i++);
 camerasTypes.forEach(type => type.aspectRatio = Math.abs(Math.tan((type.HFov/2.0) * Math.PI / 180.0)/Math.tan((type.VFov/2.0) * Math.PI / 180.0)));
+console.log(camerasTypes);
 /*
 const units = {
     meters: 1,
