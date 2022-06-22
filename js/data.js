8f11ca33d03633f3aa15efe52c34ee4682af8770
Baptiste Desgouttes <baptiste.desgouttes@gmail.com>
2022-06-22 10:52:30 +0200

    add femto

--- js/data.js
+++ js/data.js
@@ -77,7 +77,7 @@ const camerasTypes =
             "hand-tracking" 
         ],
         checkedDefault : false,
-        recommended: false
+        recommended: true
     },
     {
         id:5, 
