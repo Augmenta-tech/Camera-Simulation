33d5ca4d7b34462168000de84d2a87d9db253edc
Thomas Weissgerber <thomas.weissgerber@theoriz.com>
2023-04-21 12:11:53 +0200

    Update area fully covered icon on load

--- js/UI/UIManager.js
+++ js/UI/UIManager.js
@@ -112,6 +112,8 @@ class UIManager{
             isAreaCoveredUI(sceneManager);
             changeNumberOfNodes(sceneManager);
         }
+
+        isAreaCoveredUI(); // update area fully covered icon
     }
 }
 
