423ca5e334bd9ad5e5fb2b0b978f9f9a06ded630
Damien Chantelouve <dam@cht.lv>
2023-07-27 14:05:29 +0200

    vite conf - no assets folder

--- vite.config.js
+++ vite.config.js
@@ -9,9 +9,9 @@ export default defineConfig({
     build: {
         rollupOptions: {
             output: {
-                entryFileNames: `assets/[name].js`,
-                chunkFileNames: `assets/[name].js`,
-                assetFileNames: `assets/[name].[ext]`
+                entryFileNames: `[name].js`,
+                chunkFileNames: `[name].js`,
+                assetFileNames: `[name].[ext]`
             }
         }
     }
